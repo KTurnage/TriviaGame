106fb5c8b8cd94ff2f3408f7779e4e16dc9e3eda
KTurnage <kristaturnage314@gmail.com>
2019-03-09 16:36:04 -0700

    added win and loss images, JS variables and initial functions

---
+++ assets/javascript/app.js
@@ -0,0 +1,40 @@
+var correct = 0;
+var incorrect = 0;
+var answered = 0;
+var questionCount = 0;
+var timer = 0;
+
+var question = ["What object must be caught in order to end a Quidditch match?", "Who delivers baby Harry to Dumbledore in Privet Drive?", ""];
+var answer = ["The Golden Snitch", "Hagrid", ""];
+
+var choice1 = [];
+var choice2 = [];
+var choice3 = [];
+var choice4 = [];
+
+
+function gameStart() {
+
+}
+
+function displayQuestion() {
+
+}
+
+function correctAnswer() {
+
+}
+
+function dincorrectAnswer() {
+
+}
+
+function timeOut() {
+
+}
+
+function gameEnd() {
+    if (questionCount === question.length) {
+
+    }
+}
