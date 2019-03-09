9f4f311e2b4b4da509f2270d6db2a86a1f9bed94
KTurnage <kristaturnage314@gmail.com>
2019-03-09 16:44:24 -0700

    adding questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,8 +4,8 @@ var answered = 0;
 var questionCount = 0;
 var timer = 0;
 
-var question = ["What object must be caught in order to end a Quidditch match?", "Who delivers baby Harry to Dumbledore in Privet Drive?", ""];
-var answer = ["The Golden Snitch", "Hagrid", ""];
+var question = ["What object must be caught in order to end a Quidditch match?", "Who delivers baby Harry to Dumbledore in Privet Drive?", "Who is Harry's Godfather?", "How did Harry and Ron get to Hogwarts their second year?", "Which Longdon train station do you stand between platform 9 and 100 to catch the train to Hogwarts?", ""];
+var answer = ["The Golden Snitch", "Hagrid", "Sirius", "Flying Car", "King's Cross", ""];
 
 var choice1 = [];
 var choice2 = [];
