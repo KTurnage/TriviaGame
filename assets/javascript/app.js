e4e7105a4462f31f6eca88ae1a0fcca007c8f890
KTurnage <kristaturnage314@gmail.com>
2019-03-09 17:52:50 -0700

    adding questions and answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,17 +1,27 @@
+//Game Variables
+
 var correct = 0;
 var incorrect = 0;
 var answered = 0;
 var questionCount = 0;
 var timer = 0;
 
-var question = ["What object must be caught in order to end a Quidditch match?", "Who delivers baby Harry to Dumbledore in Privet Drive?", "Who is Harry's Godfather?", "How did Harry and Ron get to Hogwarts their second year?", "Which Longdon train station do you stand between platform 9 and 100 to catch the train to Hogwarts?", ""];
-var answer = ["The Golden Snitch", "Hagrid", "Sirius", "Flying Car", "King's Cross", ""];
+var question = ["What object must be caught in order to end a Quidditch match?", "Who delivers baby Harry to Dumbledore in Privet Drive?", "Who is Harry's Godfather?", "How did Harry and Ron get to Hogwarts their second year?", "Which Longdon train station do you stand between platform 9 and 100 to catch the train to Hogwarts?", "What magical plant does Harry use to breathe underwater?", "What color are Harry's eyes?", "Who was Harry's first kiss with?", "Who was Harry's first quidditch match against?", "What monster lives in the Chamber of Secrets?"];
+var answer = ["The Golden Snitch", "Hagrid", "Sirius", "Flying Car", "King's Cross", "Gillyweed", "Green", "Cho Chang", "Slytherin", "Basilisk"];
+
+var keyValue = [{question[i]: answer[i]}];
 
 var choice1 = [];
 var choice2 = [];
 var choice3 = [];
 var choice4 = [];
 
+//Game Functions
+$(document).ready(function() {
+    $("#click").on("clic", function() {
+        alert("I've been clicked!");
+    })
+});
 
 function gameStart() {
 
@@ -25,16 +35,53 @@ function correctAnswer() {
 
 }
 
-function dincorrectAnswer() {
+function incorrectAnswer() {
 
 }
 
-function timeOut() {
+function checkAnswer() {
+    if (question[i]:answer[i]) {
 
+    }
+    else {
+
+    }
+}
+
+function timeOut() {
+    if (timer === 0);
 }
 
 function gameEnd() {
     if (questionCount === question.length) {
 
     }
+}
+
+//Timer Functions
+
+function showTimer() {
+
+}
+
+function startTime() {
+
+}
+
+function stopTime() {
+
+}
+
+function resetTime() {
+
+}
+
+//Results
+
+function showResults() {
+
+}
+
+function resetResults() {
+
 }
