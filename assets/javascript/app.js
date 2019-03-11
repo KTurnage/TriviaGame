faf113a5e45df3f71d5c48254599c82a5284d83d
KTurnage <kristaturnage314@gmail.com>
2019-03-11 15:58:25 -0600

    JS updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,13 +3,13 @@
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
-var questionCount = 0;
 var timer = 15;
 var running = false;
 var userGuess = "";
 var intervalID;
 var pick;
 var holder = [];
+var newArray = [];
 
 
 // var question = ["What object must be caught in order to end a Quidditch match?", "Who delivers baby Harry to Dumbledore in Privet Drive?", "Who is Harry's Godfather?", "How did Harry and Ron get to Hogwarts their second year?", "Which Longdon train station do you stand between platform 9 and 100 to catch the train to Hogwarts?", "What magical plant does Harry use to breathe underwater?", "What color are Harry's eyes?", "Who was Harry's first kiss with?", "Who was Harry's first quidditch match against?", "What monster lives in the Chamber of Secrets?"];
@@ -18,16 +18,57 @@ var holder = [];
 var questionsOnPage = [
     {
         question: "What object must be caught in order to end a Quidditch match?",
-        choices: ["wrong 1", "wrong 2", "wrong 3", "The Golden Snitch"],
+        choices: ["The Ball", "Harry", "Ron", "The Golden Snitch"],
         answer: 3,
     },
     {
         question: "Who delivers baby Harry to Dumbledore in Privet Drive?",
-        choices: ["wrong 1", "Hagrid", "wrong 3", "wrong 4"],
+        choices: ["His Parents", "Hagrid", "Professor Snape", "Ron"],
         answer: 1,
     },
-
+    {
+        question: "Who is Harry's Godfather?",
+        choices: ["Sirius", "Dumbledore", "Mr. Weasley", "Professor Snape"],
+        answer: 0,
+    },
+    {
+        question: "How did Harry and Ron get to Hogwarts their second year?",
+        choices: ["Train", "Broomstick", "Teleport", "Flying Car"],
+        answer: 3,
+    },
+    {
+        question: "Which Longdon train station do you stand between platform 9 and 10 to catch the train to Hogwarts?",
+        choices: ["Grand Central", "King's Cross", "London Euston", "Paddington Station"],
+        answer: 1,
+    },
+    {
+        question: "What magical plant does Harry use to breathe underwater?",
+        choices: ["Gillyweed", "Mandrake", "Polyjuice Potion", "Snargaluff"],
+        answer: 0,
+    },
+    {
+        question: "What color are Harry's eyes?",
+        choices: ["Green", "Brown", "Blue", "Red"],
+        answer: 0,
+    },
+    {
+        question: "Who was Harry's first kiss with?",
+        choices: ["Hermione", "Ginny", "Luna", "Cho Chang"],
+        answer: 3,
+    },
+    {
+        question: "Who was Harry's first quidditch match against?",
+        choices: ["Slytherin", "Gryffindor", "Hufflepuff", "Ravenclaw"],
+        answer: 0,
+    },
+    {
+        question: "What monster lives in the Chamber of Secrets?",
+        choices: ["Dementor", "Basilisk", "Goblin", "Pixie"],
+        answer: 1,
+    },
+    
 ]
+var questionCount = questionsOnPage.length;
 
 //Game Functions
 $("#startButton").on("click", function () {
@@ -57,30 +98,34 @@ function displayQuestion() {
     $(".answerChoice").on("click", function () {
         userGuess = parseInt($(this).attr("data-guess"));
         //correct guess
+        stopTime();
         if (userGuess === pick.answer) {
-            stopTime();
             correct++;
             userGuess = "";
-            $("#answer").html("<p>CORRECT! You're helping Harry!</p>")
+            $("#answer").html("<p> CORRECT! You're helping Harry!</p>")
+            page();
         } else {
-            stop();
+            // stopTime();
             incorrect++;
             userGuess = "";
-            $("#answer").html("<p>WRONG! Stop helping Voldemort! Correct Answer is" + pick.choices[pick.answer] + "</p>");
+            $("#answer").html("<p> WRONG! Stop helping Voldemort! Correct Answer is " + pick.choices[pick.answer] + "</p>");
+            page();
         }
-
+        startTime();
     })
 };
 
 
 //to get next question to populate on the page or results page
-function Page() {
+
+function page() {
 console.log("nextPage function running")
 newArray.push(pick);
 
-var nextPage = setTimeout(function () {
+setTimeout(function () {
     $("#answer").empty();
     timer = 15;
+    displayQuestion();
 
     if ((incorrect + correct + unanswered) === questionCount) {
         console.log("checking page function if else")
@@ -97,14 +142,20 @@ var nextPage = setTimeout(function () {
         console.log(incorrect);
         unanswered = 0;
         console.log(answered);
-
+        runTimer();
 
     } else {
         runTimer();
-        displayQuestion();
+        
+        
     }
 }, 3000);
 };
+
+// function noResponse () {
+// if 
+// }
+
 
 
 //Timer Functions
